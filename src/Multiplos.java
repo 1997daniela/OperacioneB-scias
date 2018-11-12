81bf4ab93a0c3d92e941c471e6f68db56582d072
LAB7-PC1-USE <LAB7-PC1-USE@LAB7-PC1-USER>
2018-11-12 09:02:44 -0500

    Deber_12112018

---
+++ src/Multiplos.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LAB7-PC1-USE
+ */
+public class Multiplos {
+    public static void main(String[] args){
+	int numero = 0;
+	int suma = 0;
+
+	for(int i=0; i<100; i++){
+		numero ++;
+		suma = suma+ numero;
+		System.out.println("Numero atual que contenga N°"+numero +" Suma:"+suma);
+	}
+
+    }
+}
---
+++ src/NumerosPares.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author LAB7-PC1-USE
+ */
+public class NumerosPares {
+     public static void main(String[] args) {
+  //Codigo por www.DesarrollarAplicaciones.com
+ int numero=1;
+ int limite=100;
+ while(numero <= 100){
+  if(numero%2==0){
+   System.out.println(numero);
+  }
+  numero++;
+ }
+}
+}
+
+
